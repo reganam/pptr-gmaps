9564e6417f8d41e20b7f8e4f9d93c55f00c83623
Aleksandrs Kašs <aleksandrs.kass@zzdats.lv>
2021-11-29 13:03:14 +0200

    fixed issue when results was appended with old ones

--- scraper.js
+++ scraper.js
@@ -10,7 +10,7 @@ const launchOptions = {
   args: ["--disable-dev-shm-usage", "--no-sandbox"],
 };
 
-console.info(`Browser will be running in ${(launchOptions.headless ? 'headless': 'headfull')} mode`);
+console.info(`Browser will be running in ${(launchOptions.headless ? 'headless' : 'headfull')} mode`);
 
 if (process.env.PROXY_HOST && process.env.PROXY_PORT) {
   launchOptions["args"].push(
@@ -18,149 +18,155 @@ if (process.env.PROXY_HOST && process.env.PROXY_PORT) {
   );
 }
 
-let browser;
-
-// How many urls we want to process in parallel.
-const CONCURRENCY = Number(process.env.CONCURRENCY) || 5;
-console.info(`Concurrency: ${CONCURRENCY}`);
-
-let URLS = [];
-let results = [];
-
-const defaultDelay = 300; // Increase this if running on a laggy browser or device
-let debugBool = true;
-let debug = {
-  log: (...strings) => debugBool && console.log(strings.join(' ')),
-};
-
-// Get the data
-async function getPageData(url) {
-  const page = await browser.newPage();
-
-  debug.log(`Opening ${url}`);
-  await page.goto(url);
-  await page.waitForSelector('[role="main"]').catch(movingOn);
-
-  //Shop Name
-  let shopName =
-    (await page.$eval('[role="main"]', element =>
-      element.getAttribute('aria-label')
-    )) || 'No shop name provided';
-
-  //Shop Address
-  let address = await page.evaluate(() => {
-    const elem = document.querySelector('button[data-item-id="address"]')
-    if (elem) {
-      return elem.innerText
-    } else {
-      return ''
-    }
-  });  
-
-  //Website
-  let website = await page.evaluate(() => {
-    const elem = document.querySelector('[data-tooltip="Open website"]')
-    if (elem) {
-      return elem.innerText
-    } else {
-      return ''
-    }
-  });
-
-  //Phone
-  const phoneElement = (await page.$x('//button[contains(@aria-label, "Phone: ")]'))[0];
-  let phone = await page.evaluate(el => {
-    var result = el?.getAttribute('data-item-id')?.replace('phone:tel:', '')
-    
-    return (result ? result : '');
-  }, phoneElement);
-
-  let result = {
-    shop: shopName?.trim?.(),
-    address: address?.trim?.(),
-    website: website?.trim?.(),
-    phone: phone?.trim?.(),
-  };
-
-  results.push(result);
-
-  debug.log(`Closing ${url}`);
-  await page.close();
-}
-
-//Get Links
-async function getLinks(page) {
-  // Scrolling to bottom of page
-  let newScrollHeight = 0;
-  let scrollHeight = 1000;
-  let divSelector = '#pane > div > div > div > div > div:nth-child(2) > div';
-
-  debug.log('Waiting for the page to load in');
-  await page.waitForTimeout(defaultDelay * 11);
-
-  debug.log('Starting to scroll now');
-  while (true) {
-    await page.waitForSelector(divSelector).catch(error => {
-      debug.log('Unable to find results pane. Selector: ', divSelector);
-      throw new Error(error);
-    });
-
-    await page.evaluate(
-      (scrollHeight, divSelector) =>
-        document.querySelector(divSelector).scrollTo(0, scrollHeight),
-      scrollHeight,
-      divSelector
-    );
-
-    await page.waitForTimeout(defaultDelay);
-
-    newScrollHeight = await page.$eval(
-      divSelector,
-      div => div.scrollHeight
-    );
-    debug.log('scrolled by', newScrollHeight);
-
-    if (scrollHeight === newScrollHeight) {
-      break;
-    } else {
-      scrollHeight = newScrollHeight;
-    }
-  }
-  debug.log('finished scrolling');
-
-  // Get results
-  const searchResults = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('a'))
-      .map(el => el.href)
-      .filter(
-        link =>
-          link.match(/https:\/\/www.google.com\/maps\//g, link) &&
-          !link.match(/\=https:\/\/www.google.com\/maps\//g, link)
-      )
-  );
-
-  //console.log(searchResults);
-  debug.log('I got', searchResults.length, 'results');
-
-  return searchResults;
-}
-
-function movingOn() {
-  debug.log('Wait timed out, moving on...');
-}
-
-function genericMovingOn() {
-  debug.log('Recieved an error, attempting to move on...');
-}
-
-const promiseProducer = () => {
-  const url = URLS.pop();
-  
-  return url ? getPageData(url) : null;
-};
-
 class Scraper {
   async getHtml(searchQuery) {
+
+    let browser;
+
+    // How many urls we want to process in parallel.
+    const CONCURRENCY = Number(process.env.CONCURRENCY) || 5;
+    console.info(`Concurrency: ${CONCURRENCY}`);
+
+    let URLS = [];
+    let results = [];
+
+    const defaultDelay = 300; // Increase this if running on a laggy browser or device
+    let debugBool = true;
+    let debug = {
+      log: (...strings) => debugBool && console.log(strings.join(' ')),
+    };
+
+    // Get the data
+    async function getPageData(url) {
+      const page = await browser.newPage();
+
+      try {
+        debug.log(`Opening ${url}`);
+        await page.goto(url);
+        await page.waitForSelector('[role="main"]').catch(movingOn);
+
+        //Shop Name
+        let shopName =
+          (await page.$eval('[role="main"]', element =>
+            element.getAttribute('aria-label')
+          )) || 'No shop name provided';
+
+        //Shop Address
+        let address = await page.evaluate(() => {
+          const elem = document.querySelector('button[data-item-id="address"]')
+          if (elem) {
+            return elem.innerText
+          } else {
+            return ''
+          }
+        });
+
+        //Website
+        let website = await page.evaluate(() => {
+          const elem = document.querySelector('[data-tooltip="Open website"]')
+          if (elem) {
+            return elem.innerText
+          } else {
+            return ''
+          }
+        });
+
+        //Phone
+        const phoneElement = (await page.$x('//button[contains(@aria-label, "Phone: ")]'))[0];
+        let phone = await page.evaluate(el => {
+          var result = el?.getAttribute('data-item-id')?.replace('phone:tel:', '')
+
+          return (result ? result : '');
+        }, phoneElement);
+
+        let result = {
+          shop: shopName?.trim?.(),
+          address: address?.trim?.(),
+          website: website?.trim?.(),
+          phone: phone?.trim?.(),
+        };
+
+        results.push(result);
+      } catch (error) {
+        console.log(`Unable get data from ${url}`);
+        console.log(error);
+      } finally {
+        debug.log(`Closing ${url}`);
+        await page.close();
+      }
+    }
+
+    //Get Links
+    async function getLinks(page) {
+      // Scrolling to bottom of page
+      let newScrollHeight = 0;
+      let scrollHeight = 1000;
+      let divSelector = '#pane > div > div > div > div > div:nth-child(2) > div';
+
+      debug.log('Waiting for the page to load in');
+      await page.waitForTimeout(defaultDelay * 11);
+
+      debug.log('Starting to scroll now');
+      while (true) {
+        await page.waitForSelector(divSelector).catch(error => {
+          debug.log('Unable to find results pane. Selector: ', divSelector);
+          throw new Error(error);
+        });
+
+        await page.evaluate(
+          (scrollHeight, divSelector) =>
+            document.querySelector(divSelector).scrollTo(0, scrollHeight),
+          scrollHeight,
+          divSelector
+        );
+
+        await page.waitForTimeout(defaultDelay);
+
+        newScrollHeight = await page.$eval(
+          divSelector,
+          div => div.scrollHeight
+        );
+        debug.log('scrolled by', newScrollHeight);
+
+        if (scrollHeight === newScrollHeight) {
+          break;
+        } else {
+          scrollHeight = newScrollHeight;
+        }
+      }
+      debug.log('finished scrolling');
+
+      // Get results
+      const searchResults = await page.evaluate(() =>
+        Array.from(document.querySelectorAll('a'))
+          .map(el => el.href)
+          .filter(
+            link =>
+              link.match(/https:\/\/www.google.com\/maps\//g, link) &&
+              !link.match(/\=https:\/\/www.google.com\/maps\//g, link)
+          )
+      );
+
+      //console.log(searchResults);
+      debug.log('I got', searchResults.length, 'results');
+
+      return searchResults;
+    }
+
+    function movingOn() {
+      debug.log('Wait timed out, moving on...');
+    }
+
+    function genericMovingOn() {
+      debug.log('Recieved an error, attempting to move on...');
+    }
+
+    const promiseProducer = () => {
+      const url = URLS.pop();
+
+      return url ? getPageData(url) : null;
+    };
+
     if (!searchQuery) {
       throw new Error("No param: q");
     }
@@ -195,7 +201,7 @@ class Scraper {
         //await page.waitForTimeout(defaultDelay * 10);
       } catch (error) {
         console.log("The button 'I agree' didn't appear.");
-      }      
+      }
 
       while (true) {
         const nextPageDisabled = (await page.$x('//button[contains(@aria-label, " Next page ") and @disabled]'))[0];
@@ -225,11 +231,11 @@ class Scraper {
       await pool.start();
 
       results = results.filter(Boolean)
-    
+
       // Print results.
       console.log('Results:');
       console.log(JSON.stringify(results, null, 2));
-      
+
       debug.log("Scrape complete!")
 
       return results;
